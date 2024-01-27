0e21aea4dc71609d32e8dfa11f235878c7763fe7
powdevlin12 <thudatdl123@gmail.com>
2024-01-27 09:02:13 +0700

    fix scroll view

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,5 +1,11 @@
 import React, {ReactNode} from 'react';
-import {SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
 import {globalStyle} from '../../styles/global.styles';
 import theme from '../../constants/theme';
 
@@ -15,14 +21,14 @@ const Container = (props: Props) => {
   return (
     <SafeAreaView style={globalStyle.container}>
       <StatusBar barStyle={'light-content'} />
-      <ScrollView
-        contentContainerStyle={{
+      <View
+        style={{
           paddingTop: (StatusBar.currentHeight as number) + theme.size[4],
           marginHorizontal: theme.size[4],
           flex: 1,
         }}>
         {children}
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,6 +1,6 @@
 import {Element4, Notification, SearchNormal} from 'iconsax-react-native';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {TextComponent, TitleComponent} from '../../components/Text';
 import {CardComponent} from '../../components/card';
 import {
@@ -23,97 +23,103 @@ type Props = NativeStackScreenProps<AppStackParamList, 'Home'>;
 export default function Home({navigation}: Props) {
   return (
     <Container>
-      <SectionComponent>
-        <RowComponent justifyContent="space-between">
-          <Element4 size="28" color={theme.colors.desc} variant="Outline" />
-          <Notification size="28" color={theme.colors.desc} variant="Outline" />
-        </RowComponent>
-      </SectionComponent>
-      <SectionComponent>
-        <TextComponent text="Hi, Dat Tran" />
-        <TitleComponent text="Be Productive today" />
-      </SectionComponent>
-      <SectionComponent>
-        <RowComponent
-          onPress={() => console.log('Say Hiii')}
-          styles={{
-            ...globalStyle.inputContainer,
-          }}
-          justifyContent="space-between">
-          <TextComponent
-            text="Search task"
-            flex={1}
-            color={theme.colors.textOpacity}
-          />
-          <SearchNormal size="20" color={theme.colors.desc} />
-        </RowComponent>
-      </SectionComponent>
-      <SectionComponent>
-        <CardComponent>
+      <ScrollView>
+        <SectionComponent>
+          <RowComponent justifyContent="space-between">
+            <Element4 size="28" color={theme.colors.desc} variant="Outline" />
+            <Notification
+              size="28"
+              color={theme.colors.desc}
+              variant="Outline"
+            />
+          </RowComponent>
+        </SectionComponent>
+        <SectionComponent>
+          <TextComponent text="Hi, Dat Tran" />
+          <TitleComponent text="Be Productive today" />
+        </SectionComponent>
+        <SectionComponent>
+          <RowComponent
+            onPress={() => console.log('Say Hiii')}
+            styles={{
+              ...globalStyle.inputContainer,
+            }}
+            justifyContent="space-between">
+            <TextComponent
+              text="Search task"
+              flex={1}
+              color={theme.colors.textOpacity}
+            />
+            <SearchNormal size="20" color={theme.colors.desc} />
+          </RowComponent>
+        </SectionComponent>
+        <SectionComponent>
+          <CardComponent>
+            <RowComponent>
+              <View style={{flex: 1}}>
+                <TitleComponent text="Task progress" />
+                <TextComponent text="30/40 task done" />
+                <SpaceComponent height={8} />
+                <RowComponent justifyContent="flex-start">
+                  <TagComponent text="Match 22" />
+                </RowComponent>
+              </View>
+              <View>
+                <CicularComponent value={30} />
+              </View>
+            </RowComponent>
+          </CardComponent>
+        </SectionComponent>
+        <SectionComponent>
           <RowComponent>
             <View style={{flex: 1}}>
-              <TitleComponent text="Task progress" />
-              <TextComponent text="30/40 task done" />
-              <SpaceComponent height={8} />
-              <RowComponent justifyContent="flex-start">
-                <TagComponent text="Match 22" />
-              </RowComponent>
+              <ProgressTaskComponent
+                title="UX Design"
+                content="Task mamagement mobile app"
+                dueDate="Due, 12 Jun 2024"
+                progress={{
+                  percent: '70%',
+                  titleProgress: 'Doing',
+                  colorProgress: '#0aacff',
+                }}
+                group={['dat']}
+              />
             </View>
-            <View>
-              <CicularComponent value={30} />
-            </View>
-          </RowComponent>
-        </CardComponent>
-      </SectionComponent>
-      <SectionComponent>
-        <RowComponent>
-          <View style={{flex: 1}}>
-            <ProgressTaskComponent
-              title="UX Design"
-              content="Task mamagement mobile app"
-              dueDate="Due, 12 Jun 2024"
-              progress={{
-                percent: '70%',
-                titleProgress: 'Doing',
-                colorProgress: '#0aacff',
-              }}
-              group={['dat']}
-            />
-          </View>
-          <SpaceComponent width={theme.size[4]} />
-          <View style={{flex: 1}}>
-            <ProgressTaskComponent
-              title="API payment"
-              color="rgba(33,150,243,0.9)"
-              progress={{
-                percent: '36%',
-                titleProgress: 'Done',
-                colorProgress: 'rgba(18,181, 22,0.9)',
-              }}
-            />
-            <SpaceComponent height={16} />
-            <ProgressTaskComponent
-              title="Update work"
-              content="Revision home page"
-              color="rgba(18,181, 22,0.9)"
-            />
-          </View>
-        </RowComponent>
-      </SectionComponent>
-      <SectionComponent>
-        <TitleComponent text="Urgents tasks" />
-        <SpaceComponent height={theme.size[3]} />
-        <CardComponent>
-          <RowComponent justifyContent="flex-start">
-            <CicularComponent value={60} size={80} />
             <SpaceComponent width={theme.size[4]} />
-            <TitleComponent
-              text="Title of task"
-              size={theme.fontSize.paragraphLarge}
-            />
+            <View style={{flex: 1}}>
+              <ProgressTaskComponent
+                title="API payment"
+                color="rgba(33,150,243,0.9)"
+                progress={{
+                  percent: '36%',
+                  titleProgress: 'Done',
+                  colorProgress: 'rgba(18,181, 22,0.9)',
+                }}
+              />
+              <SpaceComponent height={16} />
+              <ProgressTaskComponent
+                title="Update work"
+                content="Revision home page"
+                color="rgba(18,181, 22,0.9)"
+              />
+            </View>
           </RowComponent>
-        </CardComponent>
-      </SectionComponent>
+        </SectionComponent>
+        <SectionComponent>
+          <TitleComponent text="Urgents tasks" />
+          <SpaceComponent height={theme.size[3]} />
+          <CardComponent>
+            <RowComponent justifyContent="flex-start">
+              <CicularComponent value={60} size={80} />
+              <SpaceComponent width={theme.size[4]} />
+              <TitleComponent
+                text="Title of task"
+                size={theme.fontSize.paragraphLarge}
+              />
+            </RowComponent>
+          </CardComponent>
+        </SectionComponent>
+      </ScrollView>
       <FloatButtonComponent
         onPress={() => navigation.navigate('AddNewTask')}
         title="Add new task"
